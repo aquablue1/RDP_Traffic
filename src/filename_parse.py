925a005b2701c448ef8bd609d24eea95551fb5cb
weisser <94apieceofcake@gmail.com>
2018-03-21 20:44:40 -0600

    featching real environment

--- src/filename_parse.py
+++ src/filename_parse.py
@@ -13,28 +13,17 @@ class TimePeriod(object):
         :param time_str: Input format: 21:00:00-22:00:03
         """
         # In stage, ignore the date of the time.
+        self.time_str = time_str
         if "-" in time_str and len(time_str.split("-")) == 2:
             # replace the time spliter from ":" to "_" to aviod filename mistake in Windows
             # Should be recover to ":" when applied in Linux.
-            self.start = time.strptime(time_str.split("-")[0], "%H_%M_%S")
-            self.end = time.strptime(time_str.split("-")[1], "%H_%M_%S")
+            self.start = time.strptime(time_str.split("-")[0], "%H:%M:%S")
+            self.end = time.strptime(time_str.split("-")[1], "%H:%M:%S")
         else:
             raise ValueError("Invalid Input Time Format")
 
-
-class Filename(object):
-    def __init__(self, filename):
-        # First. remove the directory info if exists.
-        filename = filename.split("/")[-1]
-        if ".log.gz" in filename:
-            filename.replace(".log", "")
-        filename_list = filename.split(".")
-
-        if len(filename_list) == 2:
-            self.protocol = filename_list[0]
-            self.time_gap = TimePeriod(filename_list[1])
-        else:
-            raise ValueError("Invalid Input Filename Format.")
+    def __str__(self):
+        return self.time_str
 
     def is_overlap(self, time_period):
         """
@@ -44,12 +33,32 @@ class Filename(object):
         :param time_period:
         :return:
         """
-        if time_period.start >= self.time_gap.start > time_period.end:
+        if time_period.end > self.start >= time_period.start :
             return True
-        elif self.time_gap.start >= time_period.start > self.time_gap.end:
+        elif self.end > time_period.start >= self.start :
             return True
         else:
             return False
+
+
+
+class Filename(object):
+    def __init__(self, filename):
+        # First. remove the directory info if exists.
+        filename = filename.split("/")[-1]
+        self.filename = filename
+        if ".log.gz" in filename:
+            filename = filename.replace(".log.gz", "")
+        filename_list = filename.split(".")
+
+        if len(filename_list) == 2:
+            self.protocol = filename_list[0]
+            self.time_gap = TimePeriod(filename_list[1])
+        else:
+            raise ValueError("Invalid Input Filename Format.")
+
+    def __str__(self):
+        return self.filename
 
 
 class TripleKey(object):
@@ -57,8 +66,17 @@ class TripleKey(object):
         str.strip()
         str_list = str.split(" ")
         if len(str_list) == 4:
-            self.sip = ipaddress.ip_address(str_list[1])
-            self.dip = ipaddress.ip_address(str_list[2])
+            self.sip = ipaddress.ip_address(str(str_list[1]))
+            self.dip = ipaddress.ip_address(str(str_list[2]))
             self.dport = str_list[3]
         else:
             raise ValueError("Invalid Input TripleKey Format.")
+
+
+if __name__ == '__main__':
+    tp1 = TimePeriod("23:00:01-00:00:12")
+    tp2 = TimePeriod("00:00:01-00:00:11")
+
+    print(tp1.is_overlap(tp2))
+
+    print(tp1.start >= tp2.start)
--- src/rdp_conn_combine.py
+++ src/rdp_conn_combine.py
@@ -17,14 +17,32 @@ def file_listing(location, key):
     selected_file_list = []
     for filename in os.listdir(location):
         if key in filename:
-            selected_file_list.append(filename)
-
+            fname_ojb = Filename(filename)
+            selected_file_list.append(fname_ojb)
     return selected_file_list
+
+
+def time_period_extract(heading_line):
+    period = None
+    if len(heading_line.split(" ")) == 2:
+        line = heading_line.split(" ")[1]
+        period = line.strip(":")
+    return TimePeriod(period)
+
+
+def output_filename_generate(source_name, triple_key):
+    simp_sour_name = source_name.replace(".log.gz", "")
+    return simp_sour_name + "-%s-%s-%s.log" % \
+                                       (str(triple_key.sip), str(triple_key.dip), str(triple_key.dport))
+
+
 
 if __name__ == '__main__':
     rdp_descriptor_filename = "../data/rdp_conn_comb/2018-02-01"
-    source_file = "../data/rdp_conn_comb/"
-    target_conn_log = None
+    source_folder = "../data/rdp_conn_comb/"
+    output_folder = "../output/"
+    conn_fname_list = file_listing(source_folder, "conn.")
+    selected_conn_log = []
     with open(rdp_descriptor_filename) as f:
         for line in f:
             line = line.strip()
@@ -38,24 +56,36 @@ if __name__ == '__main__':
             if status == "ENTER":
                 """
                 " Get Ready the Target conn filename.
-                " And the output filename
                 """
-                line = line.split(" ")[1]
-                period = line.strip(":")
-                time_period = TimePeriod(period)
-                print(line)
-                target_conn_log = "conn.00_00_00-00_00_20.log"
+                time_period = time_period_extract(line)
+                for conn_fname in conn_fname_list:
+                    if conn_fname.is_overlap(time_period):
+                        # This gives the list of conn logs that related to this time period
+                        selected_conn_log.append(str(conn_fname))
+
             elif status == "IN":
                 t_key = TripleKey(line)
-                with open(source_file + target_conn_log) as conn:
-                    for conn_line_orig in conn:
-                        if "#" in conn_line_orig:
-                            continue
-                        conn_line = conn_line_orig.strip()
-                        conn_line_list = conn_line.split("\t")
-                        if (conn_line_list[2] == str(t_key.sip) and
-                                    conn_line_list[4] == str(t_key.dip) and
-                                    conn_line_list[5] == str(t_key.dport)):
-                            print(conn_line_orig)
+                for target_conn_log in selected_conn_log:
+                    # Generate the Output filename
+                    output_filename = output_filename_generate(target_conn_log, t_key)
+                    output_file = open(output_filename, "w+")
+
+                    with open(source_folder + target_conn_log) as conn:
+                        for conn_line_orig in conn:
+                            if "#" in conn_line_orig:
+                                continue
+                            conn_line = conn_line_orig.strip()
+                            conn_line_list = conn_line.split("\t")
+                            if (conn_line_list[2] == str(t_key.sip) and
+                                        conn_line_list[4] == str(t_key.dip) and
+                                        conn_line_list[5] == str(t_key.dport)):
+                                output_file.write(conn_line_orig)
+
+            elif status == "END":
+                """
+                "  destruct parameters used in "ENTER"
+                """
+                selected_conn_log = []
+                print("==== END of Processing %s ====" % rdp_descriptor_filename)
 
 
